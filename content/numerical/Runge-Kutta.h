39f06f2ff969a7ffb2ae2a6ef1f43de2e3ad11a2
LeonvanderWaal <leon03vanderwaal@gmail.com>
2023-10-10 16:01:56 +0200

    Added Runge-Kutta

---
+++ content/numerical/Runge-Kutta.h
@@ -0,0 +1,12 @@
+/**
+ * Author: Leon van der Waal
+ * Date: 2023-10-10
+ * Description: Approximates the state of the differential equation $\frac{\mathrm{d}x}{\mathrm{d}t} = df(t,x)$ at time $t+\varepsilon$, with error margin $O(\varepsilon^4)$.
+ */
+
+template<typename T>
+T rk4(double t, T x, double dt, T df(double,T)){
+    T k1 = dt*df(t,x), k2 = dt*df(t+dt/2, x+k1/2), 
+      k3 = dt*df(t+dt/2, x+k2/2), k4 = dt*df(t+dt, x+k3);
+    return x + k1/6 + k2/3 + k3/3 + k4/6;
+}
