00cd7fb056c709c50fd8b69307dcc80340360da3
LeonvanderWaal <leon03vanderwaal@gmail.com>
2023-10-18 18:40:46 +0200

    onsegment tests

--- content/geometry/OnSegment.h
+++ content/geometry/OnSegment.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include "Point.h"
+#include "geoBoilerPlate.h"
 
 template<class P> bool onSegment(P s, P e, P p) {
 	return p.cross(s, e) == 0 && (s - p).dot(e - p) <= 0;
---
+++ stress-tests/geometry/OnSegment.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+
+#include "../../content/geometry/OnSegment.h"
+
+
+int main(){
+    typedef Point<int> P;
+    for (int i=0; i<100000; ++i){
+        int a = rand()%100-50;
+        int b = rand()%100-50;
+        int c = rand()%100-50;
+        int a2 = rand()%100-50;
+        int b2 = rand()%100-50;
+        int c2 = rand()%100-50;
+
+        assert(onSegment(pt {a,a2}, pt {b,b2}, pt {c,c2}) == onSegment(P(a,a2), P(b,b2), P(c,c2)));
+    }
+    cout << "tests passed!\n";
+}
