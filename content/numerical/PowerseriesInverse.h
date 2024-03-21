900abb68fd45204044c9b19adeb988255be30298
Jeroen Op de Beek <jeroen.opdebeek@gmail.com>
2024-03-21 21:48:05 +0100

    Add more power series stuff.

--- content/numerical/PowerseriesInverse.h
+++ content/numerical/PowerseriesInverse.h
@@ -2,22 +2,22 @@
  * Author: Leon van der Waal
  * Date: 2024-03-21
  * License: CC0
- * Source: based on inverse calculations in 
+ * Source: based on inverse calculations in cp-algorithms.com
  * Description: Calculates the inverse of the power series $(\sum a[i] x^i) \mod x^n$. $a[0]$ must be nonzero. Uses NTT, but can be swapped out for ConvMod.
  * Inputs must be in [0, mod).
  * Time: $O(N \log N)$
- * Status: untested
+ * Status: tested on Library Checker, inv of formal power series
  */
 #include "NumberTheoreticTransform.h"
-
+#pragma once
 vl inverse(vl a, int n){
     vl b = {modpow(a[0],mod-2)}, m, k;
     for (int i=1, j; i<n; i*=2){
         m = vl(a.begin(),min(a.end(),a.begin()+i*2));
         k = conv(m,b);
-        for (j=0, k[0]-=2; j<k.size(); ++j) k[j] =  (mod-k[j])%mod;
+        for (j=0, k[0]-=2; j<sz(k); ++j) k[j] =  (mod-k[j])%mod;
         b = conv(k,b);
-        b.resize(min(i*2,int(a.size())));
+        b.resize(min(i*2,n));
     }
     b.resize(n);
     return b;
---
+++ content/numerical/PowerseriesLogexp.h
@@ -0,0 +1,44 @@
+/**
+ * Author: Leon van der Waal
+ * Date: 2024-03-21
+ * License: CC0
+ * Source: based on formal power series article of cp-algorithms.com
+ * Description: Calculates Log and Exp of formal power series $(\sum a[i] x^i) \mod x^n$. $a[0]$ must be 1 for log, $0$ for exp. Uses NTT, but can be swapped out for ConvMod.
+ * Inputs must be in [0, mod).
+ * Time: $O(N \log N)$
+ * Status: Tested on Log and Exp of formal power series on Library Checker
+ */
+#include "PowerseriesInverse.h"
+#pragma once
+
+vl integrate(vl a){
+    vl b(sz(a)+1);
+    rep(i,0,sz(a)) b[i+1] = (a[i]*modpow(i+1,mod-2))%mod;
+    return b;
+}
+
+vl deriv(vl a){
+    vl b(sz(a)-1);
+    rep(i,1,sz(a)) b[i-1] = (i*a[i])%mod;
+    return b;
+}
+
+vl polylog(vl a, int n){
+    vl ad = deriv(a), ai = inverse(a,n), 
+        f = conv(ad,ai), i = integrate(f);
+    i.resize(n);
+    return i;
+}
+
+vl polyexp(vl a, int n){
+    vl q = {1}, b, c;
+    for (int i=1; i<2*n; i*=2){
+        b = vl(a.begin(), min(a.begin()+i*2, a.end()));
+        b[0] += 1;
+        c = polylog(q,sz(q));
+        rep(j,0,sz(c)) b[j] = (mod+b[j]-c[j])%mod;
+        q = conv(q,b);
+        q.resize(min(i*2,n));
+    }
+    return q;
+}
