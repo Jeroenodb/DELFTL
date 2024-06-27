35a71e73af5dedf001bb771b1bedbf1c3f700901
LeonvanderWaal <82886724+LeonvanderWaal@users.noreply.github.com>
2024-06-27 21:16:06 +0200

    Create BigBitset.h

---
+++ content/various/BigBitset.h
@@ -0,0 +1,33 @@
+/**
+ * Author: Chilli
+ * Date: 2024-06-27
+ * License: none
+ * Source: Codeforces
+ * Description: Makes bitsets $2\times$ faster
+ * Usage: Include before rest of header, even before bits/stdc++.h
+ * Status: Used a lot, works well
+ */
+#pragma once
+
+#include <string>
+#include <bits/functexcept.h>
+#include <iosfwd>
+#include <bits/cxxabi_forced.h>
+#include <bits/functional_hash.h>
+
+#pragma push_macro("__SIZEOF_LONG__")
+#pragma push_macro("__cplusplus")
+#define __SIZEOF_LONG__ __SIZEOF_LONG_LONG__
+#define unsigned unsigned long
+#define __cplusplus 201102L
+
+#define __builtin_popcountl __builtin_popcountll
+#define __builtin_ctzl __builtin_ctzll
+
+#include <bitset>
+
+#pragma pop_macro("__cplusplus")
+#pragma pop_macro("__SIZEOF_LONG__")
+#undef unsigned
+#undef __builtin_popcountl
+#undef __builtin_ctzl
