f35b29c761e230a26c882f2482549995585dbafd
Jeroen Op de Beek <jeroen.opdebeek@gmail.com>
2024-08-26 16:25:41 +0300

    Create beep.py

---
+++ content/contest/beep.py
@@ -0,0 +1,13 @@
+'''
+* Author: Jeroen Op de Beek
+* Date: 2024-08-25
+* License: CC0
+* Source: me
+* Description: Gives a notification every hour of the contest.
+* Status: Used, works well
+'''
+import time 
+from os import system
+while True: 
+    time.sleep(3600)
+    system('notify-send "Hourly Reminder" "It has been an hour!"')
