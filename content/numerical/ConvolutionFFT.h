a694a0bace3f4fa6432051ca4ba57a7c45d9b020
Jeroen Op de Beek <jeroen.opdebeek@gmail.com>
2024-10-21 01:56:24 +0200

    Put FFT in two files

---
+++ content/numerical/ConvolutionFFT.h
@@ -0,0 +1,24 @@
+/**
+ * Author: Ludo Pulles, chilli, Simon Lindholm
+ * Date: 2019-01-09
+ * License: CC0
+ * Source: http://neerc.ifmo.ru/trains/toulouse/2017/fft2.pdf (do read, it's excellent)
+   Accuracy bound from http://www.daemonology.net/papers/fft.pdf
+ * Description: Convolution using FFT.
+ * Time: O(N \log N) with $N = |A|+|B|$ ($\tilde 1s$ for $N=2^{22}$)
+ * Status: somewhat tested
+ */
+vd conv(const vd& a, const vd& b) {
+	if (a.empty() || b.empty()) return {};
+	vd res(sz(a) + sz(b) - 1);
+	int L = 32 - __builtin_clz(sz(res)), n = 1 << L;
+	vector<C> in(n), out(n);
+	copy(all(a), begin(in));
+	rep(i,0,sz(b)) in[i].imag(b[i]);
+	fft(in);
+	for (C& x : in) x *= x;
+	rep(i,0,n) out[i] = in[-i & (n - 1)] - conj(in[i]);
+	fft(out);
+	rep(i,0,sz(res)) res[i] = imag(out[i]) / (4 * n);
+	return res;
+}
