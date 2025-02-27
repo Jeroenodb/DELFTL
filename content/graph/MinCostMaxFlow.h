2bf442abcb163e02277413b7eef09429844520fd
Jeroen Op de Beek <jeroen.opdebeek@gmail.com>
2025-02-27 13:21:08 +0100

    [Fix Mincost Maxflow]

--- content/graph/MinCostMaxFlow.h
+++ content/graph/MinCostMaxFlow.h
@@ -2,46 +2,17 @@
  * Author: Stanford
  * Date: Unknown
  * Source: Stanford Notebook
-<<<<<<< HEAD
- * Description: Min-cost max-flow. cap[i][j] != cap[j][i] is allowed; double edges are not.
- *  If costs can be negative, call setpi before maxflow, but note that negative cost cycles are not supported.
- *  To obtain the actual flow, look at positive values only.
- * Status: Tested on kattis:mincostmaxflow, stress-tested against another implementation
- * Time: Approximately O(E^2)
-=======
  * Description: Min-cost max-flow.
  *  If costs can be negative, call setpi before maxflow, but note that negative cost cycles are not supported.
  *  To obtain the actual flow, look at positive values only.
  * Status: Tested on kattis:mincostmaxflow, stress-tested against another implementation
  * Time: $O(F E \log(V))$ where F is max flow. $O(VE)$ for setpi.
->>>>>>> kactl/main
  */
 #pragma once
 
 // #include <bits/extc++.h> /// include-line, keep-include
 
 const ll INF = numeric_limits<ll>::max() / 4;
-<<<<<<< HEAD
-typedef vector<ll> VL;
-
-struct MCMF {
-	int N;
-	vector<vi> ed, red;
-	vector<VL> cap, flow, cost;
-	vi seen;
-	VL dist, pi;
-	vector<pii> par;
-
-	MCMF(int N) :
-		N(N), ed(N), red(N), cap(N, VL(N)), flow(cap), cost(cap),
-		seen(N), dist(N), pi(N), par(N) {}
-
-	void addEdge(int from, int to, ll cap, ll cost) {
-		this->cap[from][to] = cap;
-		this->cost[from][to] = cost;
-		ed[from].push_back(to);
-		red[to].push_back(from);
-=======
 
 struct MCMF {
 	struct edge {
@@ -60,7 +31,6 @@ struct MCMF {
 		if (from == to) return;
 		ed[from].push_back(edge{ from,to,sz(ed[to]),cap,cost,0 });
 		ed[to].push_back(edge{ to,from,sz(ed[from])-1,0,-cost,0 });
->>>>>>> kactl/main
 	}
 
 	void path(int s) {
@@ -70,31 +40,11 @@ struct MCMF {
 
 		__gnu_pbds::priority_queue<pair<ll, int>> q;
 		vector<decltype(q)::point_iterator> its(N);
-<<<<<<< HEAD
-		q.push({0, s});
-
-		auto relax = [&](int i, ll cap, ll cost, int dir) {
-			ll val = di - pi[i] + cost;
-			if (cap && val < dist[i]) {
-				dist[i] = val;
-				par[i] = {s, dir};
-				if (its[i] == q.end()) its[i] = q.push({-dist[i], i});
-				else q.modify(its[i], {-dist[i], i});
-			}
-		};
-=======
 		q.push({ 0, s });
->>>>>>> kactl/main
 
 		while (!q.empty()) {
 			s = q.top().second; q.pop();
 			seen[s] = 1; di = dist[s] + pi[s];
-<<<<<<< HEAD
-			for (int i : ed[s]) if (!seen[i])
-				relax(i, cap[s][i] - flow[s][i], cost[s][i], 1);
-			for (int i : red[s]) if (!seen[i])
-				relax(i, flow[i][s], -cost[i][s], 0);
-=======
 			for (edge& e : ed[s]) if (!seen[e.to]) {
 				ll val = di - pi[e.to] + e.cost;
 				if (e.cap - e.flow > 0 && val < dist[e.to]) {
@@ -106,7 +56,6 @@ struct MCMF {
 						q.modify(its[e.to], { -dist[e.to], e.to });
 				}
 			}
->>>>>>> kactl/main
 		}
 		rep(i,0,N) pi[i] = min(pi[i] + dist[i], INF);
 	}
@@ -115,17 +64,6 @@ struct MCMF {
 		ll totflow = 0, totcost = 0;
 		while (path(s), seen[t]) {
 			ll fl = INF;
-<<<<<<< HEAD
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				fl = min(fl, r ? cap[p][x] - flow[p][x] : flow[x][p]);
-			totflow += fl;
-			for (int p,r,x = t; tie(p,r) = par[x], x != s; x = p)
-				if (r) flow[p][x] += fl;
-				else flow[x][p] -= fl;
-		}
-		rep(i,0,N) rep(j,0,N) totcost += cost[i][j] * flow[i][j];
-		return {totflow, totcost};
-=======
 			for (edge* x = par[t]; x; x = par[x->from])
 				fl = min(fl, x->cap - x->flow);
 
@@ -137,7 +75,6 @@ struct MCMF {
 		}
 		rep(i,0,N) for(edge& e : ed[i]) totcost += e.cost * e.flow;
 		return {totflow, totcost/2};
->>>>>>> kactl/main
 	}
 
 	// If some costs can be negative, call this before maxflow:
@@ -146,15 +83,9 @@ struct MCMF {
 		int it = N, ch = 1; ll v;
 		while (ch-- && it--)
 			rep(i,0,N) if (pi[i] != INF)
-<<<<<<< HEAD
-				for (int to : ed[i]) if (cap[i][to])
-					if ((v = pi[i] + cost[i][to]) < pi[to])
-						pi[to] = v, ch = 1;
-=======
 			  for (edge& e : ed[i]) if (e.cap)
 				  if ((v = pi[i] + e.cost) < pi[e.to])
 					  pi[e.to] = v, ch = 1;
->>>>>>> kactl/main
 		assert(it >= 0); // negative cost cycle
 	}
 };
