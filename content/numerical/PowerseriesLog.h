dc769dd99d198132e7b82add4ac381315c7b7e9f
leon <leon03vanderwaal@gmail.com>
2026-02-04 10:12:56 +0100

    split a few files in 2, for easier hashing when only a small part is used (Mo's and Powersiries log/exp).
    Also remove bigbitset from chapter

--- content/numerical/PowerseriesLog.h
+++ content/numerical/PowerseriesLog.h
@@ -29,16 +29,3 @@ vl polylog(vl a, int n){
     i.resize(n);
     return i;
 }
-
-vl polyexp(vl a, int n){
-    vl q = {1}, b, c;
-    for (int i=1; i<2*n; i*=2){
-        b = vl(a.begin(), min(a.begin()+i*2, a.end()));
-        b[0] += 1;
-        c = polylog(q,sz(q));
-        rep(j,0,sz(c)) b[j] = (mod+b[j]-c[j])%mod;
-        q = conv(q,b);
-        q.resize(min(i*2,n));
-    }
-    return q;
-}
